ff92536c3e2ca185c0cd7051e323e4cb012bf003
JSanderson <ju.a.sanderson@gmail.com>
2019-06-13 09:53:13 +0100

    create archive template, generate list of all pages to display

--- gen.js
+++ gen.js
@@ -1,15 +1,16 @@
 'use strict'
 const fs = require('fs');
 
-let template,
+let pageTemplate,
+    archiveTemplate,
     numImg;
 
 console.log('starting generator...');
 
-console.log('reading template...')
+console.log('reading page template...')
 fs.readFile('input/template-page.html', 'utf8', function(err, contents) {
   if (err) throw err
-  template = contents;
+  pageTemplate = contents;
   console.log('loading image list...');
   fs.readFile('input/images.json', 'utf8', function(err, images) {
     if (err) throw err;
@@ -17,20 +18,38 @@ fs.readFile('input/template-page.html', 'utf8', function(err, contents) {
 
     //set last page buttons on template
     numImg = images.length;
-    template = insertContent(
-      template,
+    pageTemplate = insertContent(
+      pageTemplate,
       '<!-- LATEST -->',
-      `<a href="${numImg}.html"><button>&gt;&gt; Latest</button></a>`
+      `<a href="${numImg}.html"><button>Latest &gt;&gt;</button></a>`
     );
 
     //write individual pages
     console.log('parsing images...')
+    let archiveList = [];
     images.forEach(function(img) {
       let imgPos = images.indexOf(img) + 1;
+      //write pages and copy images
       fs.writeFileSync(`output/${imgPos}.html`, writePage(img, imgPos));
       console.log(`created page ${imgPos} of ${images.length}`);
       fs.copyFileSync(`input/img/${img.img}`, `output/img/${img.img}`);
       console.log(`copied over image ${imgPos} of ${images.length}`);
+      //push to list for later archive page
+      archiveList.push(`<li><a href="${imgPos}.html">${img.title}</a></li>`)
+    });
+
+    //write archive page
+    console.log('reading archive template...')
+    fs.readFile('input/template-archive.html', 'utf8', function(err, contents) {
+      if (err) throw err
+      archiveTemplate = contents;
+      console.log('creating archive list...')
+      archiveTemplate = insertContent(
+        archiveTemplate,
+        '<!-- ARCHIVE -->',
+        archiveList.join('')
+      );
+      fs.writeFileSync(`output/archive.html`, archiveTemplate);
     });
 
     //copy over the stylesheet
@@ -42,7 +61,7 @@ fs.readFile('input/template-page.html', 'utf8', function(err, contents) {
 });
 
 function writePage(img, index) {
-  let page = template;
+  let page = pageTemplate;
   //insert title and image
   page = insertContent(page, '<!-- TITLE -->', img.title);
   page = insertContent(page, '<!-- IMG -->', `<img src="img/${img.img}">`);
@@ -58,7 +77,7 @@ function writePage(img, index) {
   page = insertContent(
     page,
     '<!-- NEXT -->',
-    `<a href="${nextPage}.html"><button>&gt; Next</button></a>`
+    `<a href="${nextPage}.html"><button>Next &gt;</button></a>`
   );
   //check for desc and render if present
   if (img.desc) {
